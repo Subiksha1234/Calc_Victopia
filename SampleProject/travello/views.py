6f09478019f980a3bb8980d1214037c40f0e57d2
Subiksha1234 <727821tuad052@skct.edu.in>
2025-11-11 13:30:50 +0530

    Implemented dynamic data rendering in travello templates

--- SampleProject/travello/views.py
+++ SampleProject/travello/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-    return render(request, "index.html")
-
-
+    destinations = [
+        {'name': 'Bali', 'price': 500},
+        {'name': 'Paris', 'price': 700},
+        {'name': 'Maldives', 'price': 900},
+        {'name': 'Tokyo', 'price': 800},
+        {'name': 'Rome', 'price': 650},
+        {'name': 'London', 'price': 750},
+        {'name': 'New York', 'price': 850},
+        {'name': 'Sydney', 'price': 950},
+        {'name': 'Cape Town', 'price': 600},
+        {'name': 'Barcelona', 'price': 700},
+        {'name': 'Venice', 'price': 720},
+        {'name': 'Bangkok', 'price': 550},
+        {'name': 'Dubai', 'price': 800},
+        {'name': 'Egypt', 'price': 500},
+        {'name': 'Mauritius', 'price': 900},
+    ]
+    return render(request, 'travello/index.html', {'destinations': destinations})
 
