5a066b13d4728ed19c312cbef0dccf39d88ca6d8
Subiksha1234 <727821tuad052@skct.edu.in>
2025-11-12 14:04:59 +0530

    DB setup and data fetch done

--- SampleProject/SampleProject/settings.py
+++ SampleProject/SampleProject/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'SampleProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'SampleDB',
+        'USER':'postgres',
+        'PASSWORD':'Subi@0604',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
@@ -117,6 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- SampleProject/travello/views.py
+++ SampleProject/travello/views.py
@@ -1,22 +1,12 @@
 from django.shortcuts import render
+from .models import Destination
 
 def index(request):
-    destinations = [
-        {'name': 'Bali', 'price': 500},
-        {'name': 'Paris', 'price': 700},
-        {'name': 'Maldives', 'price': 900},
-        {'name': 'Tokyo', 'price': 800},
-        {'name': 'Rome', 'price': 650},
-        {'name': 'London', 'price': 750},
-        {'name': 'New York', 'price': 850},
-        {'name': 'Sydney', 'price': 950},
-        {'name': 'Cape Town', 'price': 600},
-        {'name': 'Barcelona', 'price': 700},
-        {'name': 'Venice', 'price': 720},
-        {'name': 'Bangkok', 'price': 550},
-        {'name': 'Dubai', 'price': 800},
-        {'name': 'Egypt', 'price': 500},
-        {'name': 'Mauritius', 'price': 900},
-    ]
-    return render(request, 'travello/index.html', {'destinations': destinations})
+ 
+    destinations = Destination.objects.all()
 
+    for dest in destinations:
+        if dest.offer:
+            dest.desc += " (Special Offer!)"
+
+    return render(request, 'travello/index.html', {'destinations': destinations})
